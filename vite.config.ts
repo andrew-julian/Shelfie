b1eaa46d77da43bbe232d5a137f906788b25bcde
Andrew Julian <andrew@dcr.vc>
2025-10-01 05:07:58 +0000

    Attempting to reduce root in Vite build config to a simple hardcoded "client"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(import.meta.dirname, "client"),
+  //root: path.resolve(import.meta.dirname, "client"),
+  root: "client",
   plugins: [react()],
   resolve: {
     alias: {
